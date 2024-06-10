d5476fe9477bcf3e9209a151fe995a96f7bc7fb2
MadgulaVamshi06 <vamshimadgula@gmail.com>
2024-06-10 18:48:51 +0530

    updated all methods

--- index.js
+++ index.js
@@ -1,37 +1,109 @@
 const connection = require("./config/db.js");
-const express = require("express")
-const UserModel=require("./model/user.model.js");
+const express = require("express");
+const UserModel = require("./model/user.model.js");
 const ProductModel = require("./model/product.model.js");
 const server = express();
 server.use(express.json());
 
+server.get("/", (req, res) => {
+  res.send("helloworld");
+});
 
-server.get("/",(req,res)=>{
-    res.send("helloworld")
-})
 
+// user model
 
-server.post("/addusers", async (req, res) => {
-    try {
-      const { name, email,password } = req.body;
-      const user = new UserModel({
-        name,
-         email,
-         password
-      });
-      await user.save();
-      res.status(200).send(`Data recevied ${user}`);
-    } catch (error) {
-      console.log("Data not found", error);
-    }
+server.get("/products", async (req, res) => {
+  try {
+    const filter = req.query;
+    const user = await UserModel.find(filter);
+    res.status(200).send(user);
+  } catch (error) {
+    console.log("Data not found", error);
+  }
+});
+
+server.post("/users", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = new UserModel({
+    name,
+    email,
+    password,
   });
-  
+  await user.save();
+  res.send(`data recieved ${name} ${email} ${password}`);
+});
 
-server.listen(8000,async(req,res)=>{
-try {
+server.patch("/updateuser/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const updatedUser = await UserModel.findByIdAndUpdate({ _id: id }, data);
+    res.status(200).send(updatedUser);
+  } catch (error) {
+    console.log("Data not found", error);
+  }
+});
+
+server.delete("/deleteuser/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await UserModel.findByIdAndDelete({ _id: id });
+    res.status(200).send(deletedUser);
+  } catch (error) {
+    console.log("Data not found", error);
+  }
+});
+
+// product model
+
+
+server.get("/products", async (req, res) => {
+  try {
+    const filter = req.query;
+    const product = await ProductModel.find(filter);
+    res.status(200).send(product);
+  } catch (error) {
+    console.log("Data not found", error);
+  }
+});
+
+server.post("/products", async (req, res) => {
+  const { name, price, description } = req.body;
+  const product = new ProductModel({
+    name,
+    price,
+    description,
+  });
+  await product.save();
+  res.send(product);
+});
+
+server.patch("/updateproduct/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const updatedProduct = await ProductModel.findByIdAndUpdate({ _id: id }, data);
+    res.status(200).send(updatedProduct);
+  } catch (error) {
+    console.log("Data not found", error);
+  }
+});
+
+server.delete("/deleteproduct/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProduct = await ProductModel.findByIdAndDelete({ _id: id });
+    res.status(200).send(deletedProduct);
+  } catch (error) {
+    console.log("Data not found", error);
+  }
+});
+
+server.listen(8000, async (req, res) => {
+  try {
     await connection;
-    console.log("server is running on 4000 port and connected to mongo db")
-} catch (error) {
-   console.log("error",error) 
-}
-})
+    console.log("server is running on 4000 port and connected to mongo db");
+  } catch (error) {
+    console.log("error", error);
+  }
+});
